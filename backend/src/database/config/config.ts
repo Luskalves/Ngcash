49337ba75ac6cb3fd7bddd4b86b62972bb1929b3
Luskalves <lusca.alves32@gmail.com>
2022-11-14 19:00:33 -0300

    config.ts criado e configuigurado

---
+++ backend/src/database/config/config.ts
@@ -0,0 +1,19 @@
+import 'dotenv/config';
+import { Options } from 'sequelize';
+
+const {
+  DB_HOST,
+  DB_USER,
+  DB_PASS,
+  DB_NAME,
+  DB_PORT
+} = process.env;
+
+const config: Options = {
+  host: DB_HOST,
+  username: DB_USER,
+  password: DB_PASS,
+  database: DB_NAME,
+  port: Number(DB_PORT),
+  dialect: 'postgres',
+}
