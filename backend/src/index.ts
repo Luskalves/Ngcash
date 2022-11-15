fa3791b6dfc5974fbbf0d18fb8dfd4812b94801e
Luskalves <lusca.alves32@gmail.com>
2022-11-14 22:06:17 -0300

    index.ts provisorio criado

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,6 +5,12 @@ const { SERVER_PORT } = process.env;
 
 const server = express();
 
+server.get('/', (req, res) => {
+  res.status(200).json({
+    "message": "oie"
+  })
+})
+
 server.listen(SERVER_PORT, () => {
   console.log(`server on port: ${SERVER_PORT}`)
 })
