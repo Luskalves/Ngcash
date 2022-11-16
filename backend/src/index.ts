0e4b5a7e79cf585d5795a71f0759dd4a8a3fa68b
Luskalves <lusca.alves32@gmail.com>
2022-11-16 10:35:38 -0300

    rota de register e middleware de erro adcionado ao server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,19 @@
 import 'dotenv/config';
+import 'express-async-errors';
 import express from 'express';
+import registerRoute from './routes/registerRoute';
+import errorMiddleware from './middlewares/errorMiddleware';
 
 const { SERVER_PORT } = process.env;
 
 const server = express();
 
-server.get('/', (req, res) => {
-  res.status(200).json({
-    "message": "oie"
-  })
-})
+server.use(express.json())
+
+server.use('/login', )
+server.use('/register', registerRoute);
+
+server.use(errorMiddleware);
 
 server.listen(SERVER_PORT, () => {
   console.log(`server on port: ${SERVER_PORT}`)
