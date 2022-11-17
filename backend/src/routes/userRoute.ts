3f765f4abc075a5354444e747fc5a4dc9455837f
Luskalves <lusca.alves32@gmail.com>
2022-11-17 13:16:25 -0300

    rota de usuario criada

---
+++ backend/src/routes/userRoute.ts
@@ -0,0 +1,14 @@
+import { Router, Request, Response } from 'express';
+import UserModel from '../models/userModel';
+import UserService from '../services/userService';
+import UserController from '../controllers/userController';
+
+const userRoute = Router();
+
+const model = new UserModel();
+const service = new UserService(model);
+const controller = new UserController(service);
+
+userRoute.get('/', (req: Request, res: Response) => controller.getUserInfo(req, res));
+
+export default userRoute;
