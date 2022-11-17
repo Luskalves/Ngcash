ddc29551e9d806366f894f88d1dde57e1daf7260
Luskalves <lusca.alves32@gmail.com>
2022-11-17 13:17:37 -0300

    rota de transactions criada

---
+++ backend/src/routes/transactionsRoute.ts
@@ -0,0 +1,17 @@
+import { Router, Request, Response } from 'express';
+import UserModel from '../models/userModel';
+import TransactionsModel from '../models/transactionsModel';
+import TransactionsService from '../services/transactionsService';
+import TransactionsController from '../controllers/transactionsController';
+
+const transRoute = Router();
+
+const model = new TransactionsModel();
+const secondModel = new UserModel();
+const service = new TransactionsService(model, secondModel);
+const controller = new TransactionsController(service);
+
+transRoute
+transRoute.patch('/', (req: Request, res: Response) => controller.cashOut(req, res));
+
+export default transRoute;
