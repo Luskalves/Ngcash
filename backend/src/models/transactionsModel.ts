a98c5c223860093a60ef9dd6dc8392030b6dc361
Luskalves <lusca.alves32@gmail.com>
2022-11-18 16:45:47 -0300

    metodo getFiltered criado no model de transactions

--- backend/src/models/transactionsModel.ts
+++ backend/src/models/transactionsModel.ts
@@ -1,7 +1,9 @@
+import NotFound from '../errors/NotFound';
 import { Op, Sequelize } from 'sequelize';
 import Accounts from '../database/models/Accounts';
+import ITransactions from '../interfaces/ITransactions';
+import { CashType } from '../interfaces/types/CashType';
 import Transactions from '../database/models/Transactions';
-import NotFound from '../errors/NotFound';
 
 class TransactionsModel {
   public async transaction(credtId: number, debtId: number, value: number) {
@@ -17,13 +19,14 @@ class TransactionsModel {
       where: { id: debtId },
     });
 
-    const transDate = new Date();
+    let dateGenerate = new Date();
+    let newDate = dateGenerate.toISOString().split('T')[0];
 
     const transaction = await Transactions.create({
       debitedAccountId: debtId,
       creditedAccountId: credtId,
       value,
-      createdAt: transDate,
+      createdAt: newDate,
     });
 
     return transaction;
@@ -43,6 +46,34 @@ class TransactionsModel {
     if (!allTransactions) throw new NotFound("Nenhuma transação encontrada.");
 
     return allTransactions;
+  }
+
+  public async getFiltered(
+    id: number,
+    date: Date,
+    cashOp:  CashType): Promise<ITransactions[] | undefined> {
+    if (!cashOp) {
+      const filteredTransac = await Transactions.findAll({
+        where: {
+          createdAt: date,
+        }
+      });
+      return filteredTransac as ITransactions[];
+    }
+
+    let options;
+
+    switch (cashOp) {
+      case "debitedAccountId":
+        options = { where: { createdAt: date, debitedAccountId: id } };
+        break;
+      case "creditedAccountId":
+        options = { where: { createdAt: date, creditedAccountId: id } };
+        break;
+    }
+
+    const transacById = await Transactions.findAll(options);
+    return transacById as ITransactions[];
   }
 }
 
