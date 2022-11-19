c65f444a3862410da46f800212fc250335b7f8cc
Luskalves <lusca.alves32@gmail.com>
2022-11-19 18:13:29 -0300

    header de usuario implementado com novas opções

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,14 +1,34 @@
-import appContext from '../context/context';
 import React, { useContext } from 'react';
+import appContext from '../context/context';
+import { useNavigate } from 'react-router-dom';
 
 function Header() {
   const { username } = useContext(appContext);
+  
+  const navigate = useNavigate();
+
+  const logOut = () => {
+    localStorage.setItem("token", JSON.stringify(""));
+    navigate("/");
+  }
 
   return (
     <div>
       <header>
         <h1>Ngcash</h1>
+        <button
+          onClick={ () => navigate('/home')}
+        >
+          Home
+        </button>
+        <button
+          onClick={ () => navigate('/transactions')}
+        >Suas Transações</button>
+
         <div>{ username }</div>
+        <button
+          onClick={ () => logOut() }
+        >Logout</button>
       </header>
     </div>
   )
