b60377dafcfad5f1de75c947dc3c97eef550fe26
Luskalves <lusca.alves32@gmail.com>
2022-11-19 18:12:36 -0300

    alterando senhas do seeders para hashes

--- backend/src/database/seeders/20221115195605-Users.js
+++ backend/src/database/seeders/20221115195605-Users.js
@@ -6,17 +6,17 @@ module.exports = {
     await queryInterface.bulkInsert('Users', [
       {
         username: 'admin',
-        password: 'adminpass',
+        password: '25e4ee4e9229397b6b17776bfceaf8e7', // adminpass
         accountId: 1
       },
       {
         username: 'lucas',
-        password: 'senha123',
+        password: 'e7d80ffeefa212b7c5c55700e4f7193e', // senha123
         accountId: 2
       },
       {
         username: 'user',
-        password: '12345678',
+        password: '25d55ad283aa400af464c76d713c07ad', // 12345678
         accountId: 3
       }
     ])
