90c87f5bb33c7432b4fd4fb798fa38c183287934
Luskalves <lusca.alves32@gmail.com>
2022-11-20 14:25:46 -0300

    estilização adicionada ao header e botão para página de deposito adicionado

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -14,21 +14,38 @@ function Header() {
 
   return (
     <div>
-      <header>
-        <h1>Ngcash</h1>
-        <button
-          onClick={ () => navigate('/home')}
-        >
-          Home
-        </button>
-        <button
-          onClick={ () => navigate('/transactions')}
-        >Suas Transações</button>
+      <header class="flex justify-between px-5 py-5 bg-black text-white">
+        <div className="">
+          <h1 class="underline text-3xl italic">Ngcash</h1>
+        </div>
+        <div className="flex space-x-2">
+          <button
+            className="rounded p-1 hover:underline hover:bg-white hover:text-black"
+            onClick={ () => navigate('/home')}
+          >
+            Home
+          </button>
+          <button
+            className="rounded p-1 hover:underline hover:bg-white hover:text-black"
+            onClick={ () => navigate('/deposit')}
+          >
+            Depositar
+          </button>
+          <button
+            className="rounded p-1 hover:underline hover:bg-white hover:text-black"
+            onClick={ () => navigate('/transactions')}
+          >
+            Suas Transações
+          </button>
+        </div>
 
-        <div>{ username }</div>
-        <button
-          onClick={ () => logOut() }
-        >Logout</button>
+        <div className="space-x-2">
+          <span className="p-1"> { username } </span>
+          <button
+            className="p-1 rounded hover:underline hover:bg-white hover:text-black"
+            onClick={ () => logOut() }
+          >Logout</button>
+        </div>
       </header>
     </div>
   )
