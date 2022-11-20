728a7b26dc42948162ffb2dcf1c19560334b6466
Luskalves <lusca.alves32@gmail.com>
2022-11-20 15:35:02 -0300

    testes de login concluidos e mock de usuario criada

---
+++ backend/src/tests/mocks/userMocks.ts
@@ -0,0 +1,8 @@
+import IUser from "../../interfaces/IUser";
+
+const validUser: IUser = {
+  username: "validUser",
+  password: "validpass"
+}
+
+export { validUser }
---
+++ backend/src/tests/unit/models/loginModel.test.ts
@@ -0,0 +1,35 @@
+import { expect } from 'chai';
+import * as sinon from 'sinon';
+import LoginModel from '../../../models/loginModel';
+import { validUser } from '../../mocks/userMocks';
+import Users from '../../../database/models/Users';
+import NotFound from '../../../errors/NotFound';
+
+const model = new LoginModel();
+
+describe("Testes do model de login", () => {
+  beforeEach(() => {
+    sinon.restore();
+  });
+
+  it("Verifica se encontra um usuário e retorna true", async () => {
+    sinon.stub(Users, "findOne").resolves({ id: 1 } as any);
+
+    const response = await model.findOne(validUser);
+
+    expect(response).to.be.equal(true);
+  })
+
+  it("verifica se um erro é disparado ao não encontrar o usuário", async () => {
+    sinon.stub(Users, "findOne").resolves({} as any);
+    let response;
+
+    try {
+      await model.findOne(validUser);
+    } catch (error) {
+      response = error;
+    }
+
+    expect(response).to.be.instanceOf(NotFound);
+  })
+});
