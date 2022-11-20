29fd877dfd5d71062c0b47c93a93e6a0783a89a5
Luskalves <lusca.alves32@gmail.com>
2022-11-20 14:27:41 -0300

    header generico criado

---
+++ frontend/src/components/GenericHeader.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function GenericHeader() {
+  return (
+    <div>
+      <header class="px-5 py-5 bg-black text-white text-center">
+        <div className="">
+          <h1 class="underline text-5xl italic">Ngcash</h1>
+        </div>
+      </header>
+    </div>
+  )
+}
+
+export default GenericHeader;
